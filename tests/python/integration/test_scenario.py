6a0031a3786ed7425c5483021765b792a2ceb4a9
Alexandre Bourret <alexandre.bourret@dataiku.com>
2021-01-29 09:59:59 +0100

    Fix for integration

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -3,21 +3,21 @@ from dku_plugin_test_utils import dss_scenario
 TEST_PROJECT_KEY = "PLUGINTESTSHAREPOINTONLINE"
 
 
-def test_run_sharepoint_online_regular_list_write(user_clients):
-    dss_scenario.run(user_clients, project_key=TEST_PROJECT_KEY, scenario_id="RegularListWrite")
+def test_run_sharepoint_online_regular_list_write(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="RegularListWrite")
 
 
-def test_run_sharepoint_online_read_calculated_columns(user_clients):
-    dss_scenario.run(user_clients, project_key=TEST_PROJECT_KEY, scenario_id="ReadCalculatedColumns")
+def test_run_sharepoint_online_read_calculated_columns(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ReadCalculatedColumns")
 
 
-def test_run_sharepoint_online_read_long_list(user_clients):
-    dss_scenario.run(user_clients, project_key=TEST_PROJECT_KEY, scenario_id="ReadLongList")
+def test_run_sharepoint_online_read_long_list(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ReadLongList")
 
 
-def test_run_sharepoint_online_documents(user_clients):
-    dss_scenario.run(user_clients, project_key=TEST_PROJECT_KEY, scenario_id="SharePointDocuments")
+def test_run_sharepoint_online_documents(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="SharePointDocuments")
 
 
-def test_run_sharepoint_online_authentication_modes(user_clients):
-    dss_scenario.run(user_clients, project_key=TEST_PROJECT_KEY, scenario_id="AuthenticationModes")
+def test_run_sharepoint_online_authentication_modes(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="AuthenticationModes")
