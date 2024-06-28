ee83bf94a3f6b3e81f580b3f2706501735af1e3a
Alexandre Bourret <alexandre.bourret@dataiku.com>
2024-06-28 14:48:45 +0200

    [sc-191339] adding 2 new error formats

--- python-lib/sharepoint_client.py
+++ python-lib/sharepoint_client.py
@@ -773,7 +773,7 @@ class SharePointClient():
             logger.error("dump={}".format(response.content))
             enriched_error_message = self.get_enriched_error_message(response)
             if enriched_error_message is not None:
-                raise SharePointClientError("Error ({}): {}".format(calling_method, enriched_error_message))
+                raise SharePointClientError("Error {} ({}): {}".format(status_code, calling_method, enriched_error_message))
             if status_code == 400:
                 raise SharePointClientError("({}){}".format(calling_method, response.text))
             if status_code == 404:
@@ -819,7 +819,9 @@ class SharePointClient():
                 json_response,
                 [
                     ["error", "message", "value"],
-                    ["error_description"]
+                    ["error_description"],
+                    ["error","message"],
+                    ["odata.error","message","value"]
                 ]
             )
             if error_message:
